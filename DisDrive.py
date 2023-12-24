4d48607af79a56ed68f92a0f1bb0af369a61af44
Onur Kilic <kantasonurkilic@icloud.com>
2023-12-23 17:15:28 -0800

    cleanup after uploading implemented

--- DisDrive.py
+++ DisDrive.py
@@ -1,14 +1,16 @@
 ATTACHMENT_SIZE = 25000000
-TOKEN = 'MTE4ODIxMzIxMjM1MzY3OTQyMA.GDweNz.xhc3UxgjiNHGG3isto9bfvdfT7UUNypJ8_leGY'
+TOKEN = 'MTE4ODIxMzIxMjM1MzY3OTQyMA.GeeHbn.EWsMPRrLRQNfnj5ceIRE-LVrpuSDqsc6R0ZNLQ'
 
-def readFile():
-    with open(sys.argv[1], "rb") as f:
+
+def readFile(path):
+    with open(path, "rb") as f:
         contents = f.read()
     f.close()
     return contents
 
+
 def writeChunkFile(fileName, chunkNum, chunkContents):
-    chunkFile = f"output/{fileName.split('.')[0]}_{chunkNum}.txt"
+    chunkFile = f"{fileName.split('.')[0]}_{chunkNum}.txt"
     with open(chunkFile, "wb") as outF:
         outF.write(chunkContents)
 
@@ -38,29 +40,41 @@ def chunkifyFile(contents, fileName, size, numFiles):
 
     return filesToSend
 
+
 def main():
+    client = commands.Bot(command_prefix='!', intents=discord.Intents.all())
 
-    contents = readFile()
-
-    fileName = sys.argv[1].split('/')[-1]
-    size = len(contents)
-    numFiles = ceil(size / ATTACHMENT_SIZE)
-
-    out = chunkifyFile(contents, fileName, size, numFiles)
-
-    # client = commands.Bot(command_prefix = '!', intents=discord.Intents.all())
-
-    # @client.event
-    # async def on_ready():
-    #     print("The bot is ready to use!")
+    # contents = readFile()
     #
-    # @client.command(pass_context = True)
-    # async def hello(ctx):
-    #     await ctx.send(files=out)
+    # fileName = sys.argv[1].split('/')[-1]
     #
-    # client.run(TOKEN)
+    # size = len(contents)
+    # numFiles = ceil(size / ATTACHMENT_SIZE)
+    #
+    # out = chunkifyFile(contents, fileName, size, numFiles)
+
+    @client.event
+    async def on_ready():
+        print("The bot is ready to use!")
+
+    @client.command(pass_context=True)
+    async def hello(ctx, path):
+        contents = readFile(path)
+        fileName = path.split('/')[-1]
+        size = len(contents)
+        numFiles = ceil(size / ATTACHMENT_SIZE)
+
+        out = chunkifyFile(contents, fileName, size, numFiles)
+        await ctx.send(files=out)
+
+        for chunkNum in range(numFiles):
+            os.remove(f"{fileName.split('.')[0]}_{chunkNum}.txt")
+
+    client.run(TOKEN)
+
 
 if __name__ == "__main__":
+    import os
     import discord
     from discord.ext import commands
     from math import ceil
